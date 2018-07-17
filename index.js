70adf14d6f6b15dbbaeb24d1cc5ce63870569c45
Sean Christopher Febo <scf@techie.com>
2018-07-17 21:24:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+function logDriverNames(array) {
+  return array.forEach(function(element) { 
+    console.log(element.name) });
+}
+
+function logDriversByHometown(array, location) { 
+  array.forEach(function(element) { 
+    if (element.hometown === location) {
+      console.log(element.name); }
+  });
+}
+
+function driversByRevenue(array) { 
+  return array.sort()
